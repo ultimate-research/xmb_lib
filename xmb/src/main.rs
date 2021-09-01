4fd74412c307d3d5c59c64e2c2f7565be186997a
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-09-01 15:38:33 -0500

    add export support for graphviz dot files

--- xmb/src/main.rs
+++ xmb/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{App, Arg};
-use std::path::{Path, PathBuf};
+use std::{
+    io::{BufWriter, Write},
+    path::{Path, PathBuf},
+};
 use xmb_lib::xmb::Xmb;
 use xmb_lib::XmbFile;
 use xmltree::{Element, EmitterConfig};
@@ -23,7 +26,7 @@ fn main() {
                 .index(2)
                 .short("o")
                 .long("output")
-                .help("The output XML or XMB file")
+                .help("The output XML, XMB, or dot file")
                 .required(false)
                 .takes_value(true),
         )
@@ -39,47 +42,59 @@ fn main() {
             let xmb_file = XmbFile::from_xml(&element);
             let xmb = Xmb::from(&xmb_file);
 
-            print_tree(&xmb);
-
             // TODO: Just append xmb instead of adding .out?
             let output = matches
                 .value_of("output")
                 .map(|o| PathBuf::from(o))
                 .unwrap_or(PathBuf::from(input).with_extension("out.xmb"));
-            xmb.write_to_file(output).unwrap();
+
+            match output.extension().unwrap().to_str().unwrap() {
+                "xmb" => xmb.write_to_file(output).unwrap(),
+                "dot" => write_dot_graph(output, &xmb).unwrap(),
+                _ => panic!("Unsupported output extension for XML input"),
+            }
         }
         "xmb" => {
             let xmb = Xmb::from_file(input).unwrap();
-            let xmb_file = XmbFile::from(&xmb);
-
-            print_tree(&xmb);
-
-            let element = xmb_file.to_xml();
-
-            // Match the output of the original Python script where possible.
-            let config = EmitterConfig::new()
-                .perform_indent(true)
-                .indent_string("    ")
-                .pad_self_closing(false);
 
             let output = matches
                 .value_of("output")
                 .map(|o| PathBuf::from(o))
                 .unwrap_or(PathBuf::from(input).with_extension("out.xml"));
 
-            // Write the xml.
-            let mut writer = std::io::BufWriter::new(std::fs::File::create(output).unwrap());
-            element.write_with_config(&mut writer, config).unwrap();
+            match output.extension().unwrap().to_str().unwrap() {
+                "xmb" => xmb.write_to_file(output).unwrap(),
+                "xml" => {
+                    let xmb_file = XmbFile::from(&xmb);
+
+                    let element = xmb_file.to_xml();
+
+                    // Match the output of the original Python script where possible.
+                    let config = EmitterConfig::new()
+                        .perform_indent(true)
+                        .indent_string("    ")
+                        .pad_self_closing(false);
+
+                    // Write the xml.
+                    let mut writer =
+                        std::io::BufWriter::new(std::fs::File::create(output).unwrap());
+                    element.write_with_config(&mut writer, config).unwrap();
+                }
+                "dot" => write_dot_graph(output, &xmb).unwrap(),
+                _ => panic!("Unsupported output extension for XMB input"),
+            }
         }
         _ => panic!("Unsupported extension"),
     }
 }
 
-fn print_tree(xmb: &Xmb) {
+fn write_dot_graph<P: AsRef<Path>>(output: P, xmb: &Xmb) -> std::io::Result<()> {
+    let mut file = BufWriter::new(std::fs::File::create(output)?);
+
     // Print the tree structure for use with graphviz and the dot engine.
     // The output can be visualized here: https://edotor.net/
-    println!("digraph G {{");
-    println!("graph [ranksep=2];");
+    file.write_all(b"digraph G {\n")?;
+    file.write_all(b"\tgraph [ranksep=2];\n")?;
     for (i, entry) in xmb.entries.as_ref().unwrap().iter().enumerate() {
         // Add an edge from parent to child.
         if entry.parent_index != -1 {
@@ -89,39 +104,43 @@ fn print_tree(xmb: &Xmb) {
                 .unwrap()
                 .get(entry.parent_index as usize)
             {
-                // Avoid adding duplicate edges.
+                // TODO: Avoid adding duplicate edges?
                 // if parent.unk1 != i as i16 {
-                    println!(
-                        r#""{}: {}" -> "{}: {}""#,
-                        entry.parent_index,
-                        xmb.read_name(parent.name_offset).unwrap(),
-                        i,
-                        xmb.read_name(entry.name_offset).unwrap()
-                    );
+                writeln!(
+                    &mut file,
+                    "\t\"{}: {}\" -> \"{}: {}\"",
+                    entry.parent_index,
+                    xmb.read_name(parent.name_offset).unwrap(),
+                    i,
+                    xmb.read_name(entry.name_offset).unwrap()
+                )?;
                 // }
-
             }
         }
 
         // Add the unk1 edges in a different color.
         if entry.unk1 < xmb.entry_count as i16 && entry.unk1 >= 0 {
             let next_node = &xmb.entries.as_ref().unwrap()[entry.unk1 as usize];
-            println!(
-                r#""{}: {}" -> "{}: {}" [color=blue]"#,
+            writeln!(
+                &mut file,
+                "\t\"{}: {}\" -> \"{}: {}\" [color=blue]",
                 i,
                 xmb.read_name(entry.name_offset).unwrap(),
                 entry.unk1,
                 xmb.read_name(next_node.name_offset).unwrap()
-            );
+            )?;
         } else {
-            println!(
-                r#""{}: {}" -> "{}" [color=blue]"#,
+            writeln!(
+                &mut file,
+                "\t\"{}: {}\" -> \"{}\" [color=blue]",
                 i,
                 xmb.read_name(entry.name_offset).unwrap(),
                 entry.unk1
-            );
+            )?;
         }
     }
 
-    println!("}}");
+    file.write_all(b"}")?;
+    file.flush()?;
+    Ok(())
 }
